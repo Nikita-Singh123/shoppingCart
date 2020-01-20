24dde5fec8ddf3654f6fe7bb8e9c8ee779b7cd00
singh <nikita.e.singh@capgemini.com>
2020-01-20 15:00:17 +0530

    Updates

---
+++ precache-manifest.9c802776040bca6e762000cf6359c585.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81205247bf0403588223d06e87a5d91f",
+    "url": "/shoppingCart/index.html"
+  },
+  {
+    "revision": "cd15fa0fd471d393bc9c",
+    "url": "/shoppingCart/static/css/2.873418a5.chunk.css"
+  },
+  {
+    "revision": "6c5df67e61bec87e8956",
+    "url": "/shoppingCart/static/css/main.cb0c28ab.chunk.css"
+  },
+  {
+    "revision": "cd15fa0fd471d393bc9c",
+    "url": "/shoppingCart/static/js/2.68351a92.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/shoppingCart/static/js/2.68351a92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c5df67e61bec87e8956",
+    "url": "/shoppingCart/static/js/main.69d3f593.chunk.js"
+  },
+  {
+    "revision": "c9052394b8ea647f0d52",
+    "url": "/shoppingCart/static/js/runtime-main.9db48ebb.js"
+  }
+]);
